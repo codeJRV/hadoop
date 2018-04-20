f8aa0e1c611c006866ad79d9c43cbb7a5924621a
Jibin Rajan Varghese <email.jrv@gmail.com>
2018-04-20 17:11:39 -0500

    Correcting paths

--- sbin/exporters.sh
+++ sbin/exporters.sh
@@ -1,4 +1,4 @@
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
 export PATH=$PATH:$JAVA_HOME/bin
-export HADOOP_HOME=~/hadoop
-export PATH=$PATH:~/hadoop/bin
+export HADOOP_HOME=/root/hadoop
+export PATH=$PATH:/root/hadoop/bin

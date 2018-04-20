49831b08d6625483f5a59d91729f36d4e11345fa
Jibin Rajan Varghese <email.jrv@gmail.com>
2018-04-20 17:28:40 -0500

    hadoopisms

--- sbin/exporters.sh
+++ sbin/exporters.sh
@@ -1,4 +1,5 @@
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
-export PATH=$PATH:$JAVA_HOME/bin
-export HADOOP_HOME=/root/hadoop
-export PATH=$PATH:/root/hadoop/bin
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre/
+export PATH=$PATH:$JAVA_HOME/bin/
+export HADOOP_HOME=/root/hadoop/
+export PATH=$PATH:/root/hadoop/bin/
+export HADOOP_CONF_DIR='/root/hadoop/conf/'
